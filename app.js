d4562ce2e2293ee50ce3efe3034ef2ac5f2b61e1
Christina Loiacono <loiaconochristina@gmail.com>
2023-03-03 16:41:45 -0500

    Remove /tests endpoint
    This endpoint was only for testing purposes only and was not used by any clients.

--- app.js
+++ app.js
@@ -5,8 +5,6 @@
 // Import express library
 const express = require('express');
 const cors = require('cors');
-// connect to database
-const db = require('./db');
 
 // Import OUR stuff (our files, our components)
 const studentsController = require('./controllers/studentsController');
@@ -30,16 +28,5 @@ app.use('/v2/students', studentsControllerV2);
 app.get('/', (request, response) => {
   response.status(200).json({ data: 'Service is running' });
 });
-
-// Tests route for tests table in db on elephantSQL
-app.get('/tests', async (request, response) => {
-  try {
-    const tests = await db.any('SELECT * FROM tests');
-
-    response.status(200).json({ data: tests });
-  } catch (err) {
-    response.status(500).json({ error: err.message });
-  }
-})
 
 module.exports = app;
